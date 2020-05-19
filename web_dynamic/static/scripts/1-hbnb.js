331cc36836ef5a2316f143ca96aaf72e4ee4484a
sebastianchc <1200@holbertonschool.com>
2020-05-18 20:42:26 -0500

    Finallyyyy

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,3 +1,11 @@
 window.onload = function () {
-  
+  const amenityDict = {};
+  $('INPUT#amenity_check').click(function () {
+    if ($(this).prop('checked')) {
+      amenityDict[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete amenityDict[$(this).attr('data-id')];
+    }
+    $('div.amenities h4').text(Object.values(amenityDict).join(', '));
+  });
 };
