a259c17992901063d684e535a3ce5131b45aafff
sebastianchc <1200@holbertonschool.com>
2020-05-22 11:29:35 -0500

    Fetch places

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -8,43 +8,46 @@ window.onload = function () {
     }
     $('div.amenities h4').text(Object.values(amenityDict).join(', '));
   });
-
-  const url = 'http://0.0.0.0:5001/api/v1/status/';
-  $.get(url, function(data) {
-    if (data["status"] === "OK") {
-      console.log(data["status"]);
-      $("DIV#api_status").addClass("available");
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    if (data.status === 'OK') {
+      $('DIV#api_status').addClass('available');
     } else {
-      $("DIV#api_status").removeClass("available");
+      $('DIV#api_status').removeClass('available');
     }
   });
-
   $.ajax({
     url: 'http://0.0.0.0:5001/api/v1/places_search/',
     type: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    data: JSON.stringify({}),
-    success: function (result) {
-      for (const place of result) {
+    data: {},
+    success: function (places) {
+      for (const place of places) {
         const article = ['<article>',
           '<div class="title_box">',
-        `<h2>${place.name}</h2>`,
-        `<div class="price_by_night">$ ${place.price_by_night}</div>`,
-        '</div>',
-        '<div class="information">',
-        `<div class="max_guest">${place.max_guest} Guest(s)</div>`,
-        `<div class="number_rooms">${place.number_rooms} Bedroom(s)</div>`,
-        `<div class="number_bathrooms">${place.number_bathrooms} Bathroom(s)</div>`,
-        '</div>',
-        '<div class="description">',
-        `${place.description}`,
-        '</div>',
-        '</article>'];
-        $('section.places').append(article.join(''));
+          '<h2>',
+          `${place.name}`,
+          '</h2>',
+          '<div class="price_by_night">',
+          `${place.price_by_night}`,
+          '</div>',
+          '</div>',
+          '<div class="information">',
+          '<div class="max_guest">',
+          `${place.max_guest}`,
+          ' Guest(s)</div>',
+          '<div class="number_rooms">',
+          `${place.number_rooms}`,
+          ' Bedroom(s)</div>',
+          '<div class="number_bathrooms">',
+          `${place.number_bathrooms}`,
+          ' Bathroom(s)</div>',
+          '</div>',
+          '<div class="description">',
+          `${place.description}`,
+          '</div>',
+          '</article>'];
+        $('SECTION.places').append(article.join(''));
       }
-    },
-    error: function (error) {
-      console.log(error);
     }
   });
-}
+};
